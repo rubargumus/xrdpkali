c37eb6982757748f5fd65bc8436e2733b037698e
Rubar  Gümüş <61204308+rubargumus@users.noreply.github.com>
2022-04-30 11:16:03 +0300

    Add files via upload

---
+++ load.sh
@@ -0,0 +1,12 @@
+sudo apt update && sudo apt -y upgrade
+sudo apt -y install kali-desktop-xfce
+sudo apt-get install xrdp
+sudo apt-get install figlet
+sudo cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.bak
+sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
+sudo /etc/init.d/xrdp start
+clear
+figlet IT's  OK
+echo "github.com/rubargumus"
